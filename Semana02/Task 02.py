e03e4617110572eb50b168cfd74bacb6d94764e0
Guilherme <guilhermeperes0202@gmail.com>
2025-12-12 00:08:16 -0300

    Envio Task 2

---
+++ Semana02/Task 02.py
@@ -0,0 +1,33 @@
+"""Indentifiers"""
+
+# Identifiers are names used to identify variables, functions, classes, modules, and other objects in Python.
+
+# They must follow certain rules:
+# reserved words (keywords) cannot be used as identifiers:
+# True, False, None, and import, keywords like if, else, for, while, def, class, etc.
+# Must start with a letter (a-z, A-Z) or an underscore (_).
+# Can contain digits (0-9)
+# Cannot start with a digit
+
+name = 'Maria'
+age = 30
+Name = 'João'
+complet_name = 'Maria da Silva'
+
+#good practice: (clean code)
+c = 10
+count = 10
+
+s= 10 + 20
+sum
+
+#Constants
+PI = 3.14
+
+age = 18
+adult = 18
+
+if age >= adult:
+    print('You are an adult.')
+else:
+    print('You are a minor.')
