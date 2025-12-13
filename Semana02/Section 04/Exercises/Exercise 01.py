5a2b17911f9cf3ac8fa7902ec662fedd163e2d13
Guilherme <guilhermeperes0202@gmail.com>
2025-12-13 01:42:43 -0300

    Envio Exercise 1 e 2

--- Semana02/Section 04/Exercises/Exercise 01.py
+++ Semana02/Section 04/Exercises/Exercise 01.py
@@ -1,9 +1,8 @@
 #Exercise 01
 
 # Crie uma função que recebe três números como parâmetro(n1, n2, n3) e imprime na saída padrão a soma dos números
-
 def sum(n1, n2, n3):
-    return n1 + n2 + n3
+    print(n1 + n2 + n3)
 
-n1, n2, n3 = input("Digite 3 números separados por espaços: ").split()
-print(sum(n1, n2, n3))
+n1, n2, n3 = map(int, input("Digite 3 números separados por espaços: ").split())
+sum(n1, n2, n3)
--- Semana02/Section 04/Exercises/Exercise 02.py
+++ Semana02/Section 04/Exercises/Exercise 02.py
@@ -1,7 +1,9 @@
+#Exercise 02
+
 # Crie uma função que recebe três números como parâmetro(n1, n2, n3) e retorna a soma dos números
 
 def sum(n1, n2, n3):
-    print(n1 + n2 + n3)
+    return n1 + n2 + n3
 
-n1, n2, n3 = map(int, input("Digite 3 números separados por espaços: ").split())
-sum(n1, n2, n3)
+n1, n2, n3 = input("Digite 3 números separados por espaços: ").split()
+print(sum(n1, n2, n3))
