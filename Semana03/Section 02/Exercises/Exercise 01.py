5eac57b842dd9cea327fb8e5f7e1a896e27d4aac
Guilherme <guilhermeperes0202@gmail.com>
2025-12-13 23:27:44 -0300

    Envio Exercise 1

--- Semana03/Section 02/Exercises/Exercise 01.py
+++ Semana03/Section 02/Exercises/Exercise 01.py
@@ -1,26 +1,4 @@
-#Ex1.py - Carregar dados de alunos. Implemente a função:
-
-#def carregar_dados_alunos(nome_arquivo):
-    #"""Retorna uma tupla de dicionários com dados de alunos."""
-
-     #A função deve:
-    # - Receber como parâmetro o nome de um arquivo que contém dados de alunos.
-    # - Ler o arquivo linha a linha.
-    # - Para cada linha, criar um dicionário com as chaves:
-    #   - prontuario
-    #   - nome
-    #   - email
-    
-    # Retornar uma tupla, onde cada elemento é um dicionário com os dados de um aluno.
-
-    # Exemplo de arquivo de dados (texto):
-
-    # SP000001,Maria da Silva,maria@gmail.com
-
-    # SP000002,Pedro Gomes,pedro@gmail.com
-
-    # SP000003,João Santos,joao@gmail.com   
-
+#Exercício 1
 def carregar_dados_alunos(nome_arquivo):
     with open(nome_arquivo, 'r') as arquivo:
         alunos = []
