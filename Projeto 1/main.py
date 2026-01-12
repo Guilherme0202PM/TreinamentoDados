c72d848ea7d5595fe3605a2c6246ccd2f6f23bda
Guilherme <guilhermeperes0202@gmail.com>
2026-01-12 14:52:18 -0300

    Envio 7.2 Cadastrando Transacao

--- Projeto 1/main.py
+++ Projeto 1/main.py
@@ -3,6 +3,25 @@ from models import Transacao
 from repositorio_transacoes import carregar_transacoes, garantir_copia_csv, registrar_transacao
 from relatorios import exibir_transacoes, exibir_saldo
 
+def cadastrar_transacao_cli(caminho_copia):
+    print("tipo Receita(1) ou Despesa(2)")
+    op = input("Escolha: ").strip()
+    tipo = "receita" if op == "1" else "despesa"
+
+    categoria = input("Categoria: ").strip()
+    descricao = input("Descrição: ").strip()
+    data = input("Data (YYYY-MM-DD): ").strip()
+    valor = float(input("Valor: ").replace(",", ".").strip())
+
+    nova = Transacao(0, tipo, categoria, descricao, data, valor)
+    nova = registrar_transacao(caminho_copia, nova)
+
+    print(f"Transação salva com ID {nova.id}.")
+
+
+
+
+
 
 BASE_DIR = os.path.dirname(__file__)
 caminho = os.path.join(BASE_DIR, "data", "transacoes_projeto1lipai.csv")
@@ -15,17 +34,5 @@ transacoes = carregar_transacoes(caminho_copia)
 exibir_transacoes(transacoes)
 exibir_saldo(transacoes)
 
-print("tipo Receita(1) ou Despesa(2)")
-op = input("Escolha: ").strip()
+cadastrar_transacao_cli(caminho_copia)
 
-tipo = "receita" if op == "1" else "despesa"
-
-categoria = input("Categoria: ").strip()
-descricao = input("Descrição: ").strip()
-data = input("Data (YYYY-MM-DD): ").strip()
-valor = float(input("Valor: ").replace(",", ".").strip())
-
-nova = Transacao(0, tipo, categoria, descricao, data, valor)
-
-nova = registrar_transacao(caminho_copia, nova)
-print(f"Transação salva com ID {nova.id}.")
